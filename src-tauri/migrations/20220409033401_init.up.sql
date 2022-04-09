353498343b1bf22db3c49b3c54361b67b624a774
mbs-code <mbscodec@gmail.com>
2022-04-09 16:20:51 +0900

    add: migration の作成

---
+++ src-tauri/migrations/20220409033401_init.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE reports (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    body TEXT,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    deleted_at TEXT
+);
+
+CREATE TABLE tags (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    color TEXT,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+    updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE TABLE report_tag (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    report_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,25 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+use sqlx::{migrate::Migrator, SqlitePool};
+use std::path::Path;
+
+#[async_std::main]
+async fn main() {
+    // DB connection
+    let pool = SqlitePool::connect("sqlite:storage.db?mode=rwc")
+        .await
+        .unwrap();
+
+    let m = Migrator::new(Path::new("./migrations")).await.unwrap();
+    println!("{:?}", m);
+
+    let _ = m.run(&pool).await.unwrap();
+    println!("{}", "OK");
+
+    // tauri::Builder::default()
+    //   .run(tauri::generate_context!())
+    //   .expect("error while running tauri application");
 }
