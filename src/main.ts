5b22117d5a0b368fad6897383777fdfb301b210c
mbs-code <mbscodec@gmail.com>
2022-04-09 22:56:27 +0900

    add: vue-router の導入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import naive from 'naive-ui'
+import router from './router'
 import App from './App.vue'
 import './assets/css/index.scss'
 
-createApp(App).use(naive).mount('#app')
+createApp(App).use(naive).use(router).mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <pre>
+    {{ reports }}
+  </pre>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { Report, useReport } from '../composables/useReport'
+
+const reportRepo = useReport()
+const reports = ref<Report[]>([])
+
+onMounted(async () => {
+  const data = await reportRepo.getAll()
+  reports.value = data
+})
+</script>
