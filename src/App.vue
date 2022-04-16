1df45fa18723113ee51e205a60562e92fc84e737
mbs-code <mbscodec@gmail.com>
2022-04-16 23:29:56 +0900

    add: ダークモードを追加

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,29 @@
 </template> -->
 
 <template>
-  <n-dialog-provider>
-    <n-layout position="absolute">
-      <n-layout-header position="absolute" bordered style="height: 22px;">
-        <div style="background: #f0f8ff">div</div>
-      </n-layout-header>
+  <n-config-provider :theme="isDark ? darkTheme : lightTheme">
+    <n-dialog-provider>
+      <n-layout position="absolute">
+        <n-layout-header position="absolute" bordered style="height: 22px;">
+          <n-space>
+            <div>div</div>
+            <n-switch v-model:value="isDark" />
+          </n-space>
+        </n-layout-header>
 
-      <n-layout has-sider position="absolute" style="top: 22px;">
-        <n-layout content-style="padding: 8px;">
-          <router-view />
+        <n-layout has-sider position="absolute" style="top: 22px;">
+          <n-layout content-style="padding: 8px;">
+            <router-view />
+          </n-layout>
         </n-layout>
       </n-layout>
-    </n-layout>
-  </n-dialog-provider>
+    </n-dialog-provider>
+  </n-config-provider>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { lightTheme, darkTheme } from 'naive-ui'
+
+const isDark = ref<boolean>(true)
+</script>
