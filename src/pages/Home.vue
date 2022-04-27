81c21544af9ac2a6c39f256ff8fde6aeac1d47c1
mbs-code <mbscodec@gmail.com>
2022-04-28 01:03:47 +0900

    fix: report変更イベントの実装ミス修正

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,8 +7,8 @@
     <template v-for="report in reports"  :key="report.id">
       <ReportPanel
         :report="report"
-        @update="handleUpdated"
-        @delete="handleDeleted"
+        @updated="handleUpdated"
+        @deleted="handleDeleted"
       ></ReportPanel>
     </template>
 
@@ -51,6 +51,12 @@ let page = 1;
 const fetchReports = async () => {
   const tag = route.query?.tag as string // url parameter
 
+  console.log({
+    tagName: tag ?? undefined,
+    page: page,
+    count: 20,
+    latest: true
+  })
   const data = await reportAPI.getAll({
     tagName: tag ?? undefined,
     page: page,
@@ -61,14 +67,24 @@ const fetchReports = async () => {
 }
 
 // 無限スクロール処理
+const endInfinite = ref(false)
 const onInfinite  = async ({ loaded, noMore, error }: LoadAction) => {
   console.log('infinite')
+
+  // 一度 noMore になったら触らない（バグ？対策）
+  if (endInfinite.value) {
+    noMore();
+    return
+  }
 
   try {
     const data = await fetchReports()
     if (data.length === 0) {
+      console.log('nomore')
       noMore()
+      endInfinite.value = true
     } else {
+      console.log('push')
       reports.value.push(...data)
       page ++;
       loaded()
@@ -80,9 +96,11 @@ const onInfinite  = async ({ loaded, noMore, error }: LoadAction) => {
 
 // 保持リスト更新処理
 const handleCreated = (creReport: Report) => {
+  console.log('created')
   reports.value.unshift(creReport)
 }
 const handleUpdated = (updReport: Report) => {
+  console.log('updated')
   const index = reports.value.findIndex((report) => report.id === updReport.id)
   if (index >= 0) {
     // 置き換える
@@ -93,6 +111,7 @@ const handleUpdated = (updReport: Report) => {
   }
 }
 const handleDeleted = (delReport: Report) => {
+  console.log('deleted')
   const index = reports.value.findIndex((report) => report.id === delReport.id)
   if (index >= 0) {
     // 置き換える
