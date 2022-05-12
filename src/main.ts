7c939154bd4861da3f36df8b0d16398daec4cb64
mbs-code <mbscodec@gmail.com>
2022-05-12 19:23:07 +0900

    fix: naive ui は global import する必要が無かったので削除

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import naive from 'naive-ui'
 
 import router from './router'
 import App from './App.vue'
@@ -8,6 +7,5 @@ import './assets/css/index.scss'
 
 const app = createApp(App)
 app.use(createPinia())
-app.use(naive)
 app.use(router)
 app.mount('#app')
